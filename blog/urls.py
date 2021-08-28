88b60c146f3da17e43aeeee0b1611185640bc796
Shreyansh Ayanger <yanshugupta007@gmail.com>
2021-08-28 20:02:34 +0530

    day 2

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views, models
+from .views import PostDetailView
 
 urlpatterns = [
     path('', views.home,name='blog-home'),
     path('animes/', views.animelist,name='blog-list'),
     path('about_us/', views.about,name='blog-about'),
     path('contact/', views.contact,name='blog-contact'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import DetailView
 
 
 def home(request):
@@ -10,6 +11,14 @@ def animelist(request):
 		'posts':Post.objects.all()
 	}
 	return render(request,'blog/list.html', context)
+
+# def animepost(request):
+# 	return render(request,'blog/animepost.html')
+
+class PostDetailView(DetailView):
+	model=Post
+	template_name='blog/animepost.html'
+		
 
 def about(request):
     return render(request,'blog/about.html')
